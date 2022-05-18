a0a99f14aa61a91cd9dbf6596ca49ec356404f10
Sasha Amirov <amirovalex98@gmail.com>
2022-05-18 22:32:12 +0300

    fixed cors for heroku

--- index.js
+++ index.js
@@ -10,25 +10,23 @@ const app = express();
 const port = process.env.PORT || 7070;
 
 //MIDDLEWARES
-app.use(cors({ origin: "https://amirovalex.github.io" }));
+// app.use(cors({ origin: "https://amirovalex.github.io" }));
 app.use(express.json());
 app.use("/", userRoutes);
 
 //NODE SERVER
-// const http = require("http");
-// const server = http.createServer(app);
-const server = app.listen(port, () => {
-  console.log(`server is listening at http://localhost:${port}`);
-});
+const http = require("http");
+const server = http.createServer(app);
+
 //Initialize socket
 const io = require("socket.io")(server);
 
 const run = async () => {
   try {
     connectToSocket(io);
-    // server.listen(port, () => {
-    //   console.log(`server is listening at http://localhost:${port}`);
-    // });
+    server.listen(port, () => {
+      console.log(`server is listening at http://localhost:${port}`);
+    });
   } catch (error) {
     console.error(error);
   }
