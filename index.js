45993791ecef5394bd0b21fcb169f4463106ac74
Sasha Amirov <amirovalex98@gmail.com>
2022-05-18 14:54:18 +0300

    added socket io

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const userRoutes = require("./routes/user.js");
 dotenv.config();
+const userRoutes = require("./routes/user.js");
+
 const app = express();
 const port = process.env.PORT || 7070;
-
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-
 app.use("/", userRoutes);
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+// const io = new Server(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id} said ${message}`);
+  });
+});
 
 const run = async () => {
   try {
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`server is listening at http://localhost:${port}`);
     });
   } catch (error) {
